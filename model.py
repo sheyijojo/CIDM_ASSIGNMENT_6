2af97a01e2e59c2c06db0f18d2b47628cb08ad69
Olusheyi Gaji <seezie@gmail.com>
2022-03-21 19:54:39 -0500

    First Line of Code

---
+++ model.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+class Price:
+    def __init__(self):
+        self.order = None
+
+
+
+class Customer:
+    def __init__(self):
+        self.name = None
+        self.email = None
+
+
+class Order:
+    def __init__(self):
+        self.ref =None
+        self.product= None
+
+
