d4fba3048d1fa0807cc495865e79c1588025c939
Olusheyi Gaji <seezie@gmail.com>
2022-04-01 05:09:30 -0500

    Creater ner_client.py file

--- flask-ner/ner_client.py
+++ flask-ner/ner_client.py
@@ -1,6 +1,11 @@
+import spacy
+
+
 class NamedEntityClient:
-    def __init__(self):
-        pass
+    def __init__(self, model): #DIP NamedEntity client is dependent on abstraction(model) rather than low-leveldetails
+        self.model = model
+
     
     def get_ents(self, sentence):
+        #doc = self.model(sentence) - DIP in action, self.model is abstracted into Class NamedEntityClient.
         return {}
