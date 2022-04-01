0fec4249f702991fabe537388ef112bd4cd5a536
Olusheyi Gaji <seezie@gmail.com>
2022-04-01 05:11:53 -0500

    added another test file: test_ner_client.py

--- flask-ner/test/test_ner_client.py
+++ flask-ner/test/test_ner_client.py
@@ -1,5 +1,7 @@
+from pyexpat import model
 import unittest
 from ner_client import NamedEntityClient
+from test_doubles import NerModelTestDouble
 
 
 class TestNerClient(unittest.TestCase):
@@ -7,10 +9,16 @@ class TestNerClient(unittest.TestCase):
         # {ents: [{....}],
         #   html: "<span>..."}
 
-    def test_get_ents_returns_dictionary_given_empty_input(self):
-        ner = NamedEntityClient()
+    def test_get_ents_returns_dictionary_given_empty_str_causes_empty_spacy_doc_ents(self):
+        model = NerModelTestDouble('eng')
+        model.returns_doc_ents([])
+        ner = NamedEntityClient(model)
         ents = ner.get_ents("")
-        self.assertIsInstance(ents, dict) # retruns a dict from a string input
+        self.assertIsInstance(ents, dict) # returns a dict from a string input
+        #self.assertEqual(ents, {})
+    
+    def test_get_ents_returns_list_given_non_empty_string(self):
+        ner = NamedEntityClient(model)
+        ents = ner.get_ents("Sheyi is great guy")
+        self.assertIsInstance(ents, dict)
 
-
-       
