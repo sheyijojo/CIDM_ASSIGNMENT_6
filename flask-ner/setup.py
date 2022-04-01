4d685707c77bd542335e57c6864a09243ecc57fb
Olusheyi Gaji <seezie@gmail.com>
2022-04-01 02:26:27 -0500

    Added setup.py file

---
+++ flask-ner/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import run_setup
+from setuptools import find_packages, setup
+
+#settting this directory up as a package
+
+setup(
+    name= "flaskner",
+    version="0.0.1",
+    description = "A Simple NER API")
